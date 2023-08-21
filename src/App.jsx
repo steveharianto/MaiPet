9e03bc57813c5ee141a7fb8be4a2c6905c335e5c
steveharianto <steveharianto@gmail.com>
2023-08-21 12:42:51 +0700

    Create Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import "./App.css";
+import { Home } from "./Pages/Home";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-    return <div className="App">hello</div>;
+    return (
+        <div className="App">
+            hi
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,5 @@
+function Home() {
+    return <div>Hi sdjkdbjksbsdj</div>;
+}
+
+export default Home;
