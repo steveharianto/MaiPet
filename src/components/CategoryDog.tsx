4477cd848db5e77c70ff3879494c0ea77799c740
steveharianto <steveharianto@gmail.com>
2023-08-24 16:53:49 +0700

    Filter - Progress

--- src/components/CategoryDog.tsx
+++ src/components/CategoryDog.tsx
@@ -1,3 +1,28 @@
+import { useNavigate, useLocation } from "react-router-dom";
 export default function CategoryDog(props: { name: string }) {
-    return <div className="CategoryDog">{props.name}</div>;
+    const navigate = useNavigate();
+    const location = useLocation();
+    const queryParams = new URLSearchParams(location.search);
+    const filterType = queryParams.get("filterType");
+    const existingFilters = filterType ? filterType.split(",") : [];
+    const redirectToFilter = () => {
+        let updatedFilters = existingFilters;
+
+        // Check if the filter is already present, remove it; otherwise, add it
+        if (updatedFilters.includes(props.name)) {
+            updatedFilters = updatedFilters.filter((filter) => filter !== props.name);
+        } else {
+            updatedFilters.push(props.name);
+        }
+
+        // Construct the updated filterType string
+        const updatedFilterType = updatedFilters.join(",");
+
+        navigate(`/?filterType=${updatedFilterType}`);
+    };
+    return (
+        <div className="CategoryDog" onClick={redirectToFilter} style={existingFilters.includes(props.name) ? { backgroundColor: "#e0e0e0" } : {}}>
+            {props.name}
+        </div>
+    );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import CategoryDog from "../components/CategoryDog";
 import { getDocs, collection, Timestamp } from "firebase/firestore";
 import { db } from "../config/firebase";
 import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 export interface AnjingInfo {
     DOB: Timestamp | null;
     kelamin: string;
@@ -25,6 +26,10 @@ export interface Iklan {
 export default function Home() {
     const [iklanList, setIklanList] = useState<Iklan[] | null>(null);
     const iklanRef = collection(db, "iklan");
+    const location = useLocation();
+    const queryParams = new URLSearchParams(location.search);
+    const filterType = queryParams.get("filterType");
+    const existingFilters = filterType ? filterType.split(",") : [];
 
     const getIklan = async () => {
         const data = await getDocs(iklanRef);
@@ -34,45 +39,40 @@ export default function Home() {
     useEffect(() => {
         getIklan();
     }, []);
-
+    if (!iklanList) {
+        return <div>Loading...</div>;
+    }
+    const filteredIklanList =
+        existingFilters.length === 0
+            ? iklanList // Include everything if existingFilters is empty
+            : iklanList.filter((iklan) => existingFilters.includes(iklan.jenis));
+    const categories = ["Akita", "Alaskan Malamute", "American Bully", "American Cocker Spaniel", "American Pit Bull Terrier", "Australian Shepherd", "Basenji", "Beagle", "Belgian Malinois", "Bernese Mountain Dog", "Bichon Frise", "Biewer Terrier", "Border Collie", "Bulldog", "Cane Corso", "Cavalier King Charles Spaniel", "Chihuahua", "Chow Chow", "Dalmatian", "Doberman Pinscher", "Dogo Argentino", "Dutch Shepherd", "English Cocker Spaniel", "French Bulldog", "Golden Retriever", "Great Dane", "Greyhound", "Herder / German Shepherd", "Jack Russell Terrier", "Japanese Chin", "Kintamani", "Labrador Retriever", "Maltese", "Miniature Pinscher", "Miniature Schnauzer", "Mixed Breed / Campuran", "Pekingese", "Pembroke Welsh Corgi", "Pomeranian", "Poodle", "Pug", "Rottweiler", "Samoyed", "Shetland Sheepdog", "Shiba Inu", "Shih Tzu", "Siberian Husky", "Tekel / Dachshund", "West Highland White Terrier", "Yorkshire Terrier"];
     return (
         <>
             <div className="header"></div>
             <div className="content">
                 <div className="content-left">
                     <div className="filter">
-                        <p>
+                        <p style={{ padding: "0.5em", margin: "0" }}>
                             <b>Jenis Anjing</b>
                         </p>
-                        <CategoryDog name="Pomeranian" />
-                        <CategoryDog name="Akita" />
-                        <CategoryDog name="Beagle" />
-                        <CategoryDog name="Bulldog" />
-                        <CategoryDog name="Pug" />
-                        <CategoryDog name="Labrador Retriever" />
+                        {categories.map((category, index) => (
+                            <CategoryDog key={index} name={category} />
+                        ))}
                     </div>
                 </div>
                 <div className="content-right">
                     <p>
                         <b style={{ marginBottom: "0.3rem" }}>Papan Iklan</b>
                     </p>
+                    <div className="filterList">
+                        <div className="filterList-object">Pomeranian</div>
+                        <div className="filterList-object">Pomeranian</div>
+                    </div>
                     <div className="dogList">
-                        {iklanList?.map((iklan) => (
+                        {filteredIklanList?.map((iklan) => (
                             <CardDog iklan={iklan} key={iklan.id} />
                         ))}
-                        {/* <CardDog dogType={"Pomeranian"} rank={"Gold"} />
-                        <CardDog dogType={"Pomeranian"} rank={"Gold"} />
-                        <CardDog dogType={"Pomeranian"} rank={"Gold"} />
-                        <CardDog dogType={"Pomeranian"} rank={"Blue"} />
-                        <CardDog dogType={"Pomeranian"} rank={"Blue"} />
-                        <CardDog dogType={"Pomeranian"} rank={"Blue"} />
-                        <CardDog dogType={"Pomeranian"} rank={"Blue"} />
-                        <CardDog dogType={"Pomeranian"} rank={"Silver"} />
-                        <CardDog dogType={"Pomeranian"} rank={"Silver"} />
-                        <CardDog dogType={"Pomeranian"} rank={"Silver"} />
-                        <CardDog dogType={"Pomeranian"} rank={"Silver"} />
-                        <CardDog dogType={"Pomeranian"} rank={"Silver"} />
-                        <CardDog dogType={"Pomeranian"} rank={"Silver"} /> */}
                     </div>
                 </div>
             </div>
