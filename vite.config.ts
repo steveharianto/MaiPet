71e7481b1c9d8733b2e592998ada478951c012c5
steveharianto <steveharianto@gmail.com>
2023-08-30 19:42:11 +0700

    vite build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "https://github.com/steveharianto/MaiPet",
+    base: "./",
     plugins: [react()],
 });
