d10a218882b29d40482ba01ace3da6ad7d2dcac0
steveharianto <steveharianto@gmail.com>
2023-08-30 20:07:48 +0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 function App() {
+    const location = useLocation();
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/Login" element={<Login />} />
                 <Route path="/Register" element={<Register />} />
-                <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
+                <Route path="*" element={<h1>PAGE NOT FOUND {location.pathname}</h1>} />
             </Routes>
         </Router>
     );
